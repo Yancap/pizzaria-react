7f41cf5a71aa46fea6d33f8f1c18d080a543e298
Yancap <yangabriel3001f@gmail.com>
2023-02-20 12:15:33 -0300

    responsivo slide-1

--- src/pages/home/components/Slider1.js
+++ src/pages/home/components/Slider1.js
@@ -1,26 +1,42 @@
 import React from 'react'
+import styled from 'styled-components'
 import { Btn } from '../../../components/common/Btn'
 import { Card } from '../../../components/common/Card'
 import { TitleH2 } from '../../../components/common/TitleH2'
 import { TitleH3 } from '../../../components/common/TitleH3'
 import { FooterSlide } from './FooterSlide'
 
+const ContainerDesktop = styled.div`
+  @media (max-width: 992px) {
+    display: none;
+  }
+`
+
+
 export const Slider1 = () => {
   return (
     <div className='carousel-item active slider-item-1 container-fluid pt-5 position-relative '>
-        <TitleH2 name="Nosso Menu" className='title title-carousel'/>
-        <div className='card-container d-flex col-9 justify-content-between '>
-            <Card title='Pizzas' subtitle={null} className='pizza '/>
-            <Card title='Massas' subtitle={null} className='massa'/>
-            <Card title='Sobremesas' subtitle={null} className='sobremesa'/>
-            <Card title='Drinks' subtitle={null} className='drink'/>
-        </div>
+        
+        <ContainerDesktop>
+          <TitleH2 name="Nosso Menu" className='title title-carousel'/>
+          <div className='card-container d-flex col-xl-10 col-12  justify-content-between flex-wrap'>
+              <Card title='Pizzas' subtitle={null} className='pizza '/>
+              <Card title='Massas' subtitle={null} className='massa'/>
+              <Card title='Sobremesas' subtitle={null} className='sobremesa'/>
+              <Card title='Drinks' subtitle={null} className='drink'/>
+          </div>
+        </ContainerDesktop>
+
         <TitleH3 name='Rodizio + Refil livre' className='text-display text-warning mt-5' style={{fontSize: '2.5rem'}}/>
-        <div className='d-flex col-12 justify-content-between'>
+        <div className='d-flex col-12 justify-content-between flex-wrap'>
             <TitleH3 name='R$ 69,00' className='text-display text-primary price'/>
-            <Btn name='FAÇA SUA RESERVA' link='#' classBtn='btn btn-warning px-5 py-3 align-self-center' classAnchor='text-danger text-display anchor-btn'/>
+            <Btn name='FAÇA SUA RESERVA' link='#' classBtn='btn btn-warning px-5 py-3 align-self-center flex-shrink-0' classAnchor='text-danger text-display anchor-btn'/>
         </div>
-        <FooterSlide name="Oferta válida apenas em finais de semana" className='text-primary position-absolute bottom-0'/>
+        <ContainerDesktop>
+          <FooterSlide name="Oferta válida apenas em finais de semana" className='text-primary position-absolute bottom-0'/>
+        </ContainerDesktop>
+        
+
     </div>
   )
 }
