e96895407767dbf55a830be0e487d572d0f91007
Yancap <yangabriel3001f@gmail.com>
2023-02-24 12:05:20 -0300

    responsivo da pagina nosso espaço

--- src/components/common/LabelImg.js
+++ src/components/common/LabelImg.js
@@ -3,9 +3,9 @@ import React from 'react'
 export const LabelImg = ({text}) => {
   return (
     <div className='label d-flex justify-content-center align-items-center position-relative'>
-        <div className='text-display'>
-            <h1>{text}</h1>
-        </div>
+        
+      <h1 className='text-display'>{text}</h1>
+
     </div>
   )
 }
--- src/pages/espace-page/Espace.js
+++ src/pages/espace-page/Espace.js
@@ -4,6 +4,16 @@ import { GridFooter } from './utility/GridFooter'
 import { GridTop } from './utility/GridTop'
 import './css/Espace.css'
 import { Head } from '../Head'
+import styled from 'styled-components'
+
+const Paragraph = styled.p`
+  font-weight: 700;
+  width: 60vw;
+  @media screen and (max-width: 992px){
+    width: initial;
+    font-size: 1em;
+  }
+  `
 
 export const Espace = () => {
   return (
@@ -11,10 +21,10 @@ export const Espace = () => {
       <Head title="Nosso Espaço" />
         <LabelImg text="NOSSO ESPAÇO" />
         <div className='container-fluid'>
-          <p className='paragraph text py-5' style={{width: '60vw'}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+          <Paragraph className='text py-5' >Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
           Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
           when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-          It has survived not only five centuries</p>
+          It has survived not only five centuries</Paragraph>
           <div className='grid-espace mb-5'>
               <div className='image espace-1'></div>
               <div className='image espace-2'></div>
